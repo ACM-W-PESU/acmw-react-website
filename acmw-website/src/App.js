f1ea5a7c456493de2c7b0a1d709c00465d421dcf
divyarao54 <110017858+divyarao54@users.noreply.github.com>
2024-10-07 22:59:28 +0530

    Add files via upload

--- acmw-website/src/App.js
+++ acmw-website/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to ACMW Website</h1>
+      <Routes>
+        <Route path="/home" element={<Navbar />} />
+        <Route exact path="/" element={<Navigate to="/home" />} />
+        <Route path="/about" element={<Navigate to="/home" />} />
+        <Route path="/events" element={<Navigate to="/home" />} />
+        <Route path="/gallery" element={<Navigate to="/home" />} />
+      </Routes>
     </div>
   );
 }
