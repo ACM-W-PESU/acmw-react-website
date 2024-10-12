2eeec629fd1b568fa96d96b645aef80118c738d4
shreyyy12 <138242158+shreyyy12@users.noreply.github.com>
2024-10-12 23:18:30 +0530

    added home

--- acmw-website/src/App.js
+++ acmw-website/src/App.js
@@ -3,15 +3,17 @@ import { Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/navbar';
 import MissionVisionSection from './missionvisionobj';
 import './App.css';
+import HomePage from './home';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage/>} />
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/about" element={<Navigate to="/home" />} />
+        <Route path="/about" element={<Navigate to="MissionVisionSection" />} />
         <Route path="/events" element={<Navigate to="/home" />} />
         <Route path="/gallery" element={<Navigate to="/home" />} />
       </Routes>
